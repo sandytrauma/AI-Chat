77b24c1c0e95840d7cc5060d11c0d76e96594cc2
sandytrauma <sksandeep443gmail.com>
2024-09-09 17:11:57 +0530

    config for netlify updated

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    // Target serverless deployment
+    target: 'serverless',
+    // Optional: you can enable experimental features or other settings here
+    experimental: {
+      // Enables React Strict Mode
+      reactStrictMode: true,
+    },
+  };
+  
+  export default nextConfig;
+  
