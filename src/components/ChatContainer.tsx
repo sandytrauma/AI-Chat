6e148de88b13e70c92b87c48df68b6bf6706bf47
sandytrauma <sksandeep443gmail.com>
2024-09-11 16:57:23 +0530

    fetch messages included in dependency array

--- src/components/ChatContainer.tsx
+++ src/components/ChatContainer.tsx
@@ -9,7 +9,7 @@ import { db, ref, set, query, orderByChild, limitToLast, get } from '../services
 import { FaCircleNotch } from "react-icons/fa";
 import Link from "next/link"
 import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet"
-import { Badge } from "@/components/ui/badge"
+
 import { Button } from "@/components/ui/button"
 
 import {
@@ -17,11 +17,10 @@ import {
   Home,
   LineChart,
   Menu,
-  Package,
+
   Package2,
 
-  ShoppingCart,
-  Users,
+
 } from "lucide-react"
 
 import {
@@ -87,7 +86,7 @@ const ChatContainer: React.FC = () => {
     return () => {
       clearInterval(intervalId); // Clean up interval on component unmount
     };
-  }, []);
+  }, [fetchMessages]);
 
   useEffect(() => {
     endOfMessagesRef.current?.scrollIntoView({ behavior: 'smooth' });
@@ -194,36 +193,13 @@ const ChatContainer: React.FC = () => {
               <Home className="h-5 w-5" />
               Dashboard
             </Link>
-            <Link
-              href="#"
-              className="mx-[-0.65rem] flex items-center gap-4 rounded-xl bg-muted px-3 py-2 text-foreground hover:text-foreground"
-            >
-              <ShoppingCart className="h-5 w-5" />
-              Orders
-              <Badge className="ml-auto flex h-6 w-6 shrink-0 items-center justify-center rounded-full">
-                6
-              </Badge>
-            </Link>
-            <Link
-              href="#"
-              className="mx-[-0.65rem] flex items-center gap-4 rounded-xl px-3 py-2 text-muted-foreground hover:text-foreground"
-            >
-              <Package className="h-5 w-5" />
-              Products
-            </Link>
-            <Link
-              href="#"
-              className="mx-[-0.65rem] flex items-center gap-4 rounded-xl px-3 py-2 text-muted-foreground hover:text-foreground"
-            >
-              <Users className="h-5 w-5" />
-              Customers
-            </Link>
+            
             <Link
               href="#"
               className="mx-[-0.65rem] flex items-center gap-4 rounded-xl px-3 py-2 text-muted-foreground hover:text-foreground"
             >
               <LineChart className="h-5 w-5" />
-              Analytics
+              Usage
             </Link>
           </nav>
           <div className="mt-auto">
